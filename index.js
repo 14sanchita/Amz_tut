cf5c1aaba4834b8407839e15fd6fb9d8fe6e9d17
Sanchita Tewary <sanchitatewary03@gmail.com>
2025-04-09 16:37:41 +0530

    is above 18

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const age = 20;
-const name ='sanchita tewary'
+const name1 ='sanchita tewary'
 const dob = '01/01/2004'
 function printname(){
-    console.log('my name is '+name)
+    console.log('my name is '+name1)
+}
+function ageAbove18(){
+    return age>=18
 }
